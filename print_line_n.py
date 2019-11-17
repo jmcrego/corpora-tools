15863cfb5398ec2963be52b2f72698009ec1c9f6
Josep Crego <crego@systran.fr>
2019-11-17 17:10:33 +0100

    print_line_n using sed

--- print_line_n.py
+++ print_line_n.py
@@ -3,6 +3,8 @@
 import sys
 import glob
 import numpy as np
+import os
+import subprocess
 from collections import OrderedDict
 prog = sys.argv.pop(0)
 if len(sys.argv) < 2 or not sys.argv[-1].isdigit():
@@ -11,5 +13,8 @@ if len(sys.argv) < 2 or not sys.argv[-1].isdigit():
 n = int(sys.argv.pop()) ### 0 outputs the last line of files
 F = np.concatenate([glob.glob(o) for o in sys.argv])
 F = list(OrderedDict.fromkeys(F).keys())
-for f in F: sys.stdout.write('{}\t{}'.format(f,open(f,'r').readlines()[n-1]))
-
+#for f in F: sys.stdout.write('{}\t{}'.format(f,open(f,'r').readlines()[n-1]))
+for f in F:
+    getLine = subprocess.Popen("sed '{}q;d' {}".format(n,f), shell=True, stdout=subprocess.PIPE).stdout #sed "1000q;d" file
+    line =  getLine.read().rstrip()
+    sys.stdout.write('{}\t{}\n'.format(f,line))
