8d638a8bc3fe17fdb7e1a5aa8f4c92373fd70e61
Josep Crego <crego@systran.fr>
2019-12-13 10:19:34 +0100

    lcs

--- matches2input.cpp
+++ matches2input.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <cstdlib>
 #include <vector>
-#include<set>
+#include <set>
 
 std::vector<std::string> split(std::string str, std::string sep, bool allow_empty) {
   std::vector<std::string> v;
@@ -21,77 +21,79 @@ std::vector<std::string> split(std::string str, std::string sep, bool allow_empt
   return v;
 }
 
-std::vector<std::pair<size_t,size_t> > LCS(std::vector<std::string> X, std::vector<std::string> Y) {
-  std::vector<std::pair<size_t,size_t> > result;
-  if (X.size() == 0 || Y.size() == 0) return result;
-  std::string last_x = X.back();
-  std::string last_y = Y.back();
-  
-  if (last_x == last_y) {
-    X.pop_back(); 
-    Y.pop_back();
-    result = LCS(X, Y);
-    result.push_back(std::make_pair(X.size(),Y.size()));
-    return result;
+std::vector<std::pair<size_t,size_t> > LCS(std::vector<std::string> X, std::vector<std::string> Y,bool verbose){
+  size_t x = X.size();
+  size_t y = Y.size();
+  size_t L[x+1][y+1];
+  //find lcs
+  for (int i=0; i<=x; i++) {
+    for (int j=0; j<=y; j++) {
+      if (i == 0 || j == 0) L[i][j] = 0;
+      else if (X[i-1] == Y[j-1]) L[i][j] = L[i-1][j-1] + 1;
+      else L[i][j] = std::max(L[i-1][j], L[i][j-1]);
+    }
   }
-  
-  X.pop_back();
-  std::vector<std::pair<size_t,size_t> > v1 = LCS(X, Y);
-  
-  X.push_back(last_x);
-  Y.pop_back();
-  std::vector<std::pair<size_t,size_t> > v2 = LCS(X, Y);
-  
-  if (v1.size() > v2.size()) return v1;
-  else return v2;
+  std::vector<std::pair<size_t,size_t> > lcs_xy;
+  // Start from the right-most-bottom-most corner to recover lcs
+  while (x > 0 && y > 0) {
+    // if same, part of the result
+    if (X[x-1] == Y[y-1]) {
+      lcs_xy.insert(lcs_xy.begin(),std::make_pair(x-1,y-1));
+      if (verbose) std::cout << "lcs " << x-1 << " " << y-1 << std::endl;
+      x--;
+      y--;
+    }
+    // If not same, then find the larger of two and go in the direction of larger value
+    else if (L[x-1][y] > L[x][y-1]) x--;
+    else y--;
+  }
+  return lcs_xy;
 }
 
-std::vector<std::pair<size_t,size_t> > ALI(std::vector<std::string> A) {
-  std::vector<std::pair<size_t,size_t> > result;
-  if (A.size() == 0) return result;
+std::vector<std::pair<size_t,size_t> > ALI(std::vector<std::string> A, bool verbose) {
+  std::vector<std::pair<size_t,size_t> > ali_xy;
+  if (A.size() == 0) return ali_xy;
   for (size_t i=0; i<A.size(); i++){
     std::vector<std::string> st = split(A[i], "-", false);
     if (st.size() != 2 ){
       std::cerr << "error: bad alignment format: "<< A[i] << std::endl; 
       exit(1);
     }
-    result.push_back(std::make_pair(std::atoi(st[0].c_str()),std::atoi(st[1].c_str())));
+    ali_xy.push_back(std::make_pair(std::atoi(st[0].c_str()),std::atoi(st[1].c_str())));
+    if (verbose) std::cout << "ali " << st[0] << " " << st[1] << std::endl;
   }
-  return result;
+  return ali_xy;
 }
 
-void lcs_ali(std::vector<std::string> X, std::vector<std::string> S, std::vector<std::string> T, std::vector<std::string> A, std::vector<bool>& x_related, std::vector<bool>& t_related, bool verbose) {
-  std::vector<std::pair<size_t,size_t> > lcs_xs = LCS(X,S);
-  std::vector<std::pair<size_t,size_t> > ali_st = ALI(A);
+void related(std::vector<std::string> X, std::vector<std::string> S, std::vector<std::string> T, std::vector<std::string> A, std::vector<bool>& x_related, std::vector<bool>& t_related, bool verbose) {
+  std::vector<std::pair<size_t,size_t> > lcs_xs = LCS(X,S,verbose);
+  std::vector<std::pair<size_t,size_t> > ali_st = ALI(A,verbose);
   std::set<size_t> myset;
 
-  //build s2x (vector of ints)
+  //build s2x
   std::vector<std::set<size_t> > s2x(S.size(),myset); //fill of empty sets
   for (size_t i = 0; i < lcs_xs.size(); i++) {
     s2x[lcs_xs[i].second].insert(lcs_xs[i].first);
   }
-  //build t2s (vector of sets)
+  //build t2s
   std::vector<std::set<size_t> > t2s(T.size(),myset); //fill of empty sets
   for (size_t i = 0; i < ali_st.size(); i++) {
     t2s[ali_st[i].second].insert(ali_st[i].first);
   }
 
   for (size_t t=0 ; t<T.size(); t++){
-    bool all_related = true; //all alignments of this t must be to s which are related to x
-    for (std::set<size_t>::iterator it=t2s[t].begin(); it!=t2s[t].end(); ++it){
-      size_t s = *it;
-      if (s2x[s].size() == 0){
+    bool all_related = true; //all alignments of this t must be aligned to s's related to x
+    for (std::set<size_t>::iterator it_s=t2s[t].begin(); it_s!=t2s[t].end(); ++it_s){
+      if (s2x[*it_s].size() == 0){ //this s is not related to any x
         all_related = false;
         break;  
       }
     }   
     if (all_related && t2s[t].size() > 0){ //at least one alignment
       t_related[t] = true;
-      for (std::set<size_t>::iterator it1=t2s[t].begin(); it1!=t2s[t].end(); ++it1){
-        size_t s = *it1;
-        for (std::set<size_t>::iterator it2=s2x[s].begin(); it2!=s2x[s].end(); ++it2){
-          size_t x = *it2;
-          x_related[x] = true;
+      for (std::set<size_t>::iterator it_s=t2s[t].begin(); it_s!=t2s[t].end(); ++it_s){
+        for (std::set<size_t>::iterator it_x=s2x[*it_s].begin(); it_x!=s2x[*it_s].end(); ++it_x){
+          x_related[*it_x] = true;
         }
       }
     }
@@ -121,7 +123,7 @@ void usage(std::string name){
   return;
 }
 
-bool load(std::string file, std::vector<std::string> v){
+bool load(std::string file, std::vector<std::string>& v){
   std::string str;
   std::ifstream in(file.c_str());
   if(!in) { 
@@ -222,43 +224,63 @@ int main(int argc, char** argv) {
   of2.open((fout+".f2").c_str(), std::ofstream::out);
 
   for (size_t i = 0; i< vtst.size(); i++){
+    if (verbose){
+      std::cout << "i=" << i << std::endl;
+      std::cout << "match: " << vmatch[i] << std::endl;
+      std::cout << "tst: " << vtst[i] << std::endl;
+    }
     std::vector<std::string> X = split(vtst[i],sep,false);
     std::vector<std::string> cols = split(vmatch[i],"\t",false);
-    size_t j = std::atoi(cols[col].c_str());
-    if (j >= vsrc.size()){
-      std::cerr << "error: match out of bounds!" << std::endl;
-      return 1;
-    }
-    std::vector<std::string> S = split(vsrc[j],sep,false);      
-    std::vector<std::string> T = split(vtgt[j],sep,false);      
-    std::vector<std::string> A = split(vali[j],sep,false);      
-    std::vector<bool> x_related(X.size(),false);
-    std::vector<bool> t_related(T.size(),false);
-    lcs_ali(X,S,T,A,x_related,t_related,verbose);
-
-    for (size_t i=0; i<X.size(); i++){
-      of1 << (i?" ":"") << X[i];
-      if (x_related[i]){
-	of2 << (i?" ":"") << "C";
-      }
-      else{
-	of2 << (i?" ":"") << "S";
+    std::vector<std::string> vf1;
+    std::vector<std::string> vf2;
+    /*** there is no match ********************/
+    if (cols.size() < col+1){
+      for (size_t i=0; i<X.size(); i++){
+	vf1.push_back(X[i]);
+	vf2.push_back("S");
       }
     }
-    of1 << " " << "@";
-    of2 << " " << "@";
-    for (size_t i=0; i<T.size(); i++){
-      of1 << " " << T[i];
-      if (t_related[i]){
-	of2 << " " <<  "T";
+    /*** there is a match ********************/
+    else {
+      size_t j = std::atoi(cols[col].c_str()) - 1;
+      if (j >= vsrc.size()){
+	std::cerr << "error: match out of bounds!" << std::endl;
+	return 1;
       }
-      else{
-	of2 << " " << "U";
+      std::vector<std::string> S = split(vsrc[j],sep,false);
+      std::vector<std::string> T = split(vtgt[j],sep,false);
+      std::vector<std::string> A = split(vali[j],sep,false);
+      if (verbose) {
+	std::cout << "S: " << vsrc[j] << std::endl;
+	std::cout << "T: " << vtgt[j] << std::endl;
+	std::cout << "A: " << vali[j] << std::endl;
       }
+      std::vector<bool> x_related(X.size(),false);
+      std::vector<bool> t_related(T.size(),false);
+      related(X,S,T,A,x_related,t_related,verbose);
+      for (size_t i=0; i<X.size(); i++){
+	vf1.push_back(X[i]);
+	if (x_related[i]) vf2.push_back("C"); else vf2.push_back("S");
+      }
+      vf1.push_back("@");
+      vf2.push_back("@");
+      for (size_t i=0; i<T.size(); i++){
+	vf1.push_back(T[i]);
+	if (t_related[i]) vf2.push_back("T"); else vf2.push_back("U");
+      }
+    }
+    //write senetnces in corresponding files
+    if (verbose) std::cout << "AUGMENTED:";
+    for (size_t i=0; i<vf1.size(); i++){
+      of1 << (i?" ":"") << vf1[i];
+      of2 << (i?" ":"") << vf2[i];
+      if (verbose) std::cout << " " << vf1[i] << " " << vf2[i];
     }
     of1 << std::endl;
     of2 << std::endl;
+    if (verbose) std::cout << std::endl;
   }
+
   of1.close();
   of2.close();
 
