d7706fe7c736e837bc91d655d55030c80c0df085
Josep Crego <crego@systran.fr>
2019-12-16 16:08:41 +0100

    light

--- include/Align.h
+++ include/Align.h
@@ -7,6 +7,6 @@ public:
   std::vector<std::set<size_t> > y2x;
   std::vector<std::pair<size_t,size_t> > ali_xy;
   Align(std::vector<std::string> A,size_t,size_t);
-  std::vector<std::pair<std::set<size_t>, std::set<size_t>> > Group(bool);
+  std::vector<std::pair<std::set<size_t>, std::set<size_t> > > Group(bool);
 };
 
