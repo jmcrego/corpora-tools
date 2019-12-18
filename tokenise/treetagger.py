14010b2eeb342071178a293bf1b45e5918938576
Josep Crego <crego@systran.fr>
2019-12-18 20:55:08 +0100

    light

--- tokenise/treetagger.py
+++ tokenise/treetagger.py
@@ -11,20 +11,21 @@
 import sys
 import treetaggerwrapper as ttpw
 
+name = sys.argv.pop(0)
+tagdir = ttpw.locate_treetagger()
+lang = 'en'
+sep = '￨'
 usage = """usage: {} [-l LANG] [-sep STRING] < stdin > stdout
-   -l     LANG : use: en, de, fr, es, ca (default en)
-   -sep STRING : use STRING as separator (default ￨)
-   -tagdir DIR : treetagger dir absolute path (default /home/crego/progs/tree-tagger)
+   -l     LANG : use: en, de, fr, es, ca (default {})
+   -sep STRING : use STRING as separator (default {})
+   -tagdir DIR : treetagger dir absolute path (default {})
    -h               : this message
 Comments:
 - A light tokenization is performed as first step (similar to (onmt) tokenize -m conservative
 - Use python3
 - Needs treetaggerwrapper
-""".format(sys.argv.pop(0))
+""".format(name,lang,sep,tagdir)
 
-lang = 'en'
-sep = '￨'
-tagdir = None
 while len(sys.argv):
     tok = sys.argv.pop(0)
     if tok=="-l" and len(sys.argv):
@@ -45,20 +46,18 @@ while len(sys.argv):
         sys.stderr.write("{}".format(usage))
         sys.exit()
 
-sys.stderr.write('lang={} sep={} tagdir={}\n'.format(lang,sep,tagdir))
-if tagdir is None:
-    tagger = ttpw.TreeTagger(TAGLANG=lang) ### TAGDIR is not needed if accessible
-else:
-    tagger = ttpw.TreeTagger(TAGLANG=lang, TAGDIR=tagdir)
+if tagdir == "":
+    sys.stderr.write('error: tree-tagger location was not found\n')
+    sys.exit()
+
+sys.stderr.write('lang=\'{}\' sep=\'{}\' tagdir=\'{}\'\n'.format(lang,sep,tagdir))
+tagger = ttpw.TreeTagger(TAGLANG=lang, TAGDIR=tagdir)
 
 for n,line in enumerate(sys.stdin):
-    line = line.rstrip()
-    #toks = ttpw.make_tags(tagger.tag_text(line))
-    toks = tagger.tag_text(line,notagurl=True,notagemail=True,notagip=True,notagdns=True,nosgmlsplit=True)
+    toks = tagger.tag_text(line.rstrip(),notagurl=True,notagemail=True,notagip=True,notagdns=True,nosgmlsplit=True)
     for t in range(len(toks)):
         tags = toks[t].split('\t')
         if len(tags) != 3:
             sys.stderr.write('warning: line={} tags={}\n'.format(n+1,' '.join(tags)))
         toks[t] = sep.join(tags)
     print(' '.join(toks))
-    #if (len(line.split(' ')) != len(toks)): sys.stderr.write('warning: diff num of tokens in line={}\n{}\n{}\n'.format(n+1,line,' '.join(toks)))
