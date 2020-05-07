00b2024ead6456798b61698b42419e17485a7d06
CREGO Josep <SYSTRAN\crego@ssaling09.systran.local>
2020-05-07 10:20:39 +0200

    multipole lines

--- corpus/print_line_n.py
+++ corpus/print_line_n.py
@@ -1,26 +1,31 @@
-#!/usr/bin/python -u
 # -*- coding: utf-8 -*-
-import sys
-import os
-import glob
-import numpy as np
-import subprocess
-from collections import OrderedDict
-prog = sys.argv.pop(0)
-if len(sys.argv) < 2 or not sys.argv[-1].isdigit():
-    sys.stderr.write('usage: {} [file]+ INT\n'.format(prog))
-    sys.exit()
-n = int(sys.argv.pop()) ### 0 outputs the last line of files
-F = np.concatenate([glob.glob(o) for o in sys.argv])
-F = list(OrderedDict.fromkeys(F).keys())
-#for f in F: sys.stdout.write('{}\t{}'.format(f,open(f,'r').readlines()[n-1]))
-for f in F:
-    cmd = "sed '{}q;d' {}".format(n,f)
-    getLine = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout #sed "1000q;d" file
-    line =  getLine.read().rstrip()
 
-    #cmd = "mapfile -s 100000 -n 1 ary < {}; printf '%s' \"${}ary[0]{}\"".format(f,'{','}')
-    #getLine = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout
-    #line =  getLine.read().rstrip()
+import sys
+import glob
+import subprocess
+
+prog = sys.argv.pop(0)
+if len(sys.argv) < 2:
+    sys.stderr.write('usage: {} [FILE]+ [INT]+\n'.format(prog))
+    sys.stderr.write('  FILE : file to consider (wildcards can be used)\n')
+    sys.stderr.write('   INT : line number to output\n')
+    sys.exit()
+
+F = []
+N = []
+while len(sys.argv):
+    o = sys.argv.pop(0)
+    if o.isdigit():
+        N.append(int(o))
+    else:
+        for f in glob.glob(o):
+            F.append(f)
+        
+for f in F:
+    for n in N:
+        cmd = "sed '{}q;d' {}".format(n,f)
+        getLine = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout #sed "1000q;d" file
+        line =  getLine.read().rstrip().decode('utf-8')
+        sys.stdout.write('{}[{}]\t{}\n'.format(f,n,line))
+
     
-    sys.stdout.write('{}\t{}\n'.format(f,line))
