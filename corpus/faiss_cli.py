c21c6b7b7f259a28cf352b4d0945795e54d847c3
jmcrego <jmcrego@gmail.com>
2020-09-07 18:53:03 +0200

    multiple files

--- corpus/faiss_cli.py
+++ corpus/faiss_cli.py
@@ -82,7 +82,7 @@ class IndexFaiss:
         self.index.add(self.db.vec) #add all normalized vectors to the index
         tend = timer()
         sec_elapsed = (tend - tstart)
-        vecs_per_sec = len(self.index) / sec_elapsed
+        vecs_per_sec = len(self.db.vec) / sec_elapsed
         logging.info('Read db with {} vectors in {} sec [{:.2f} vecs/sec]'.format(self.index.ntotal, sec_elapsed, vecs_per_sec))
 
     def Query(self,file,file_str,k,min_score,skip_same_id):
