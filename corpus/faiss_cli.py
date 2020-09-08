2ba70e6b292d0cb5b574ec168334e127d5a53cdf
jmcrego <jmcrego@gmail.com>
2020-09-08 11:05:36 +0200

    multiple files

--- corpus/faiss_cli.py
+++ corpus/faiss_cli.py
@@ -127,6 +127,7 @@ class IndexFaiss:
                 for j in range(len(I[i_query])):
                     i_db = I[i_query,j]
                     score = D[i_query,j]
+                    print(score,i_db)
                     if score < min_score: ### skip
                         continue
                     if skip_same_id and i_query == i_db: ### skip
