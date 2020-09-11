d5ab2b7f853be0c5f891bac2b39ec3673890a5c6
jmcrego <jmcrego@gmail.com>
2020-09-11 12:34:02 +0000

    tokenize

--- tokenise/tokenize.py
+++ tokenise/tokenize.py
@@ -4,70 +4,17 @@ import sys
 import os
 import six
 import pyonmttok
+from tokenizer import tokenizer
 
-def build_tokenizer(args):
-    #sys.stderr.write("Options = {}\n".format(args))
-    local_args = {}
-    for k, v in six.iteritems(args):
-        if isinstance(v, six.string_types):
-            local_args[k] = v.encode('utf-8')
-        else:
-            local_args[k] = v
-    mode = local_args['mode']
-    del local_args['mode']
-    return pyonmttok.Tokenizer(mode, **local_args)
-
-
-def tokenize(tokopts, fin, fout, num_threads):
-    t = build_tokenizer(tokopts)
-
-    if fin == 'stdin' and fout == 'stdout':
-        for line in sys.stdin:
-            line, _ = t.tokenize(str(line.strip('\n')))
-            print("{}".format(" ".join(line)))
-
-    elif fin != 'stdin' and fout != 'stdout':
-        t.tokenize_file(input_path=fin, output_path=fout, num_threads=num_threads)
-
-    else:
-        sys.stderr.write('error: options -i/-o must both be used when no stdin/stdout\n')
-        sys.stderr.write("{}\n".format(usage))
-        sys.exit()
-    
 ################################################
 ### MAIN #######################################
 ################################################
 
 if __name__ == '__main__':
-
-    tokopts = {
-        'mode': "conservative",
-        'bpe_model_path': "",
-        'bpe_dropout': 0.0,
-        'vocabulary_path': "",
-        'vocabulary_threshold': 0,
-        'sp_model_path': "",
-        'sp_nbest_size': 0,
-        'sp_alpha': 0.1,
-        'joiner': "￭",
-        'joiner_annotate': False,
-        'joiner_new': False,
-        'spacer_annotate': False,
-        'spacer_new': False,
-        'case_feature': False,
-        'case_markup': False,
-        'no_substitution': False,
-        'preserve_placeholders': False,
-        'preserve_segmented_tokens': False,
-        'segment_case': False,
-        'segment_numbers': False,
-        'segment_alphabet_change': False,
-        'support_prior_joiners': False,
-        'segment_alphabet': []
-    }
     fin = 'stdin'
     fout = 'stdout'
     num_threads = 1
+    T = tokenizer()
 
     usage = """usage: {} [-i FILE -o FILE -num_threads INT]  [tok_options]
     -i: stdin
@@ -76,15 +23,9 @@ if __name__ == '__main__':
     -h: this message
 
   tok_options (See https://github.com/OpenNMT/Tokenizer for more details):
-""".format(sys.argv.pop(0),tokopts)
-    for k,v in tokopts.items():
+""".format(sys.argv.pop(0),T.tokopts)
+    for k,v in T.tokopts.items():
         usage += "    -{}: {}\n".format(k,v)
-
-    opt_strings = ['mode','bpe_model_path','vocabulary_path','sp_model_path','joiner']
-    opt_ints = ['vocabulary_threshold','sp_nbest_size']
-    opt_floats = ['bpe_dropout','sp_alpha']
-    opt_lists = ['segment_alphabet']
-    opt_bools = ['joiner_annotate','joiner_new','spacer_annotate','spacer_new','case_feature','case_markup','no_substitution','preserve_placeholders','preserve_segmented_tokens','segment_case','segment_numbers','segment_alphabet_change','support_prior_joiners']
 
     while len(sys.argv):
         tok = sys.argv.pop(0)
@@ -99,16 +40,16 @@ if __name__ == '__main__':
             sys.stderr.write("{}\n".format(usage))
             sys.exit()
 
-        if tok in opt_strings and len(sys.argv):
-    	    tokopts[tok] = sys.argv.pop(0)
-        elif tok in opt_ints and len(sys.argv):
-    	    tokopts[tok] = int(sys.argv.pop(0))
-        elif tok in opt_floats and len(sys.argv):
-    	    tokopts[tok] = float(sys.argv.pop(0))
-        elif tok in opt_lists and len(sys.argv):
-    	    tokopts[tok] = sys.argv.pop(0).split(',')
-        elif tok in opt_bools:
-    	    tokopts[tok] = True
+        if tok in T.opt_strings and len(sys.argv):
+    	    T.tokopts[tok] = sys.argv.pop(0)
+        elif tok in T.opt_ints and len(sys.argv):
+    	    T.tokopts[tok] = int(sys.argv.pop(0))
+        elif tok in T.opt_floats and len(sys.argv):
+    	    T.tokopts[tok] = float(sys.argv.pop(0))
+        elif tok in T.opt_lists and len(sys.argv):
+    	    T.tokopts[tok] = sys.argv.pop(0).split(',')
+        elif tok in T.opt_bools:
+    	    T.tokopts[tok] = True
         elif tok=="i" and len(sys.argv):
             fin = sys.argv.pop(0)
         elif tok=="o" and len(sys.argv):
@@ -120,4 +61,17 @@ if __name__ == '__main__':
             sys.stderr.write("{}\n".format(usage))
             sys.exit()
 
-    tokenize(tokopts, fin, fout, num_threads)
+    if fin == 'stdin' and fout == 'stdout':
+        for line in sys.stdin:
+            line = str(line.strip('\n'))
+            line = T.tokenize_string(line)
+            print("{}".format(" ".join(line)))
+
+    elif fin != 'stdin' and fout != 'stdout':
+        T.tokenize_file(fin, fout, num_threads)
+
+    else:
+        sys.stderr.write('error: options -i/-o must both be used when no stdin/stdout\n')
+        sys.stderr.write("{}\n".format(usage))
+        sys.exit()
+    
--- tokenise/tokenizer.py
+++ tokenise/tokenizer.py
@@ -1,26 +1,64 @@
 # -*- coding: utf-8 -*-
 
-import sys
 import os
+import sys
 import six
 import pyonmttok
 
-def build_tokenizer(args):
-    """Builds a tokenizer based on user arguments."""
-    local_args = {}
-    for k, v in six.iteritems(args):
-        if isinstance(v, six.string_types):
-            local_args[k] = v.encode('utf-8')
-        else:
-            local_args[k] = v
-
-    if not 'mode' in local_args:
-        sys.stderr.write('error: missing mode in tokenizer options\n')
-        sys.exit()
+class tokenizer():
+    def __init__(self):
+        self.T = None #my tokenizer
+        self.tokopts = {
+            'mode': "conservative",
+            'bpe_model_path': "",
+            'bpe_dropout': 0.0,
+            'vocabulary_path': "",
+            'vocabulary_threshold': 0,
+            'sp_model_path': "",
+            'sp_nbest_size': 0,
+            'sp_alpha': 0.1,
+            'joiner': "￭",
+            'joiner_annotate': False,
+            'joiner_new': False,
+            'spacer_annotate': False,
+            'spacer_new': False,
+            'case_feature': False,
+            'case_markup': False,
+            'no_substitution': False,
+            'preserve_placeholders': False,
+            'preserve_segmented_tokens': False,
+            'segment_case': False,
+            'segment_numbers': False,
+            'segment_alphabet_change': False,
+            'support_prior_joiners': False,
+            'segment_alphabet': []
+        }
+        self.opt_strings = ['mode','bpe_model_path','vocabulary_path','sp_model_path','joiner']
+        self.opt_ints = ['vocabulary_threshold','sp_nbest_size']
+        self.opt_floats = ['bpe_dropout','sp_alpha']
+        self.opt_lists = ['segment_alphabet']
+        self.opt_bools = ['joiner_annotate','joiner_new','spacer_annotate','spacer_new','case_feature','case_markup','no_substitution','preserve_placeholders','preserve_segmented_tokens','segment_case','segment_numbers','segment_alphabet_change','support_prior_joiners']
         
-    mode = local_args['mode']
-    del local_args['mode']
-    if 'vocabulary' in local_args: 
-        del local_args['vocabulary']
+    def build_tokenizer(self):
+        #sys.stderr.write("Options = {}\n".format(self.tokopts))
+        local_args = {}
+        for k, v in six.iteritems(self.tokopts):
+            if isinstance(v, six.string_types):
+                local_args[k] = v.encode('utf-8')
+            else:
+                local_args[k] = v
+        mode = local_args['mode']
+        del local_args['mode']
+        self.T = pyonmttok.Tokenizer(mode, **local_args)
 
-    return pyonmttok.Tokenizer(mode, **local_args)
+    def tokenize_file(self,fin, fout, num_threads):
+        if self.T == None:
+            self.build_tokenizer()
+        self.T.tokenize_file(input_path=fin, output_path=fout, num_threads=num_threads)
+
+    def tokenize_string(self,line):
+        if self.T == None:
+            self.build_tokenizer()
+        line, _ = self.T.tokenize(line)
+        return line
+        
