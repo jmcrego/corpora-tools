ca3b7adc7a441b95844f32f8e40d26615de5b7c9
jmcrego <jmcrego@gmail.com>
2020-09-11 13:58:14 +0000

    light

--- tokenise/subword_learn.py
+++ tokenise/subword_learn.py
@@ -3,121 +3,85 @@
 import sys
 import os
 import six
+import glob 
 import pyonmttok
+import logging
+from tokenizer import tokenizer
 
-def build_tokenizer(args):
-    #sys.stderr.write("Options = {}\n".format(args))
-    local_args = {}
-    for k, v in six.iteritems(args):
-        if isinstance(v, six.string_types):
-            local_args[k] = v.encode('utf-8')
-        else:
-            local_args[k] = v
-    mode = local_args['mode']
-    del local_args['mode']
-    return pyonmttok.Tokenizer(mode, **local_args)
-
-
-def tokenize(tokopts, fin, fout, num_threads):
-    t = build_tokenizer(tokopts)
-
-    if fin == 'stdin' and fout == 'stdout':
-        for line in sys.stdin:
-            line, _ = t.tokenize(str(line.strip('\n')))
-            print("{}".format(" ".join(line)))
-
-    elif fin != 'stdin' and fout != 'stdout':
-        t.tokenize_file(input_path=fin, output_path=fout, num_threads=num_threads)
-
-    else:
-        sys.stderr.write('error: options -i/-o must both be used when no stdin/stdout\n')
-        sys.stderr.write("{}\n".format(usage))
+def create_logger(logfile=None, loglevel='debug'):
+    numeric_level = getattr(logging, loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        sys.stderr.write("Invalid log level={}\n".format(loglevel))
         sys.exit()
-    
+    if logfile is None or logfile == 'stderr':
+        logging.basicConfig(format='[%(asctime)s.%(msecs)03d] %(levelname)s %(message)s', datefmt='%Y-%m-%d_%H:%M:%S', level=numeric_level)
+        logging.info('Created Logger level={}'.format(loglevel))
+    else:
+        logging.basicConfig(filename=logfile, format='[%(asctime)s.%(msecs)03d] %(levelname)s %(message)s', datefmt='%Y-%m-%d_%H:%M:%S', level=numeric_level)
+        logging.info('Created Logger level={} file={}'.format(loglevel, logfile))
+
+
 ################################################
 ### MAIN #######################################
 ################################################
 
 if __name__ == '__main__':
+    fin = []
+    fout = None
+    symbols = 32000
+    min_frequency = 1
+    t = tokenizer()
 
-    tokopts = {
-        'mode': "conservative",
-        'bpe_model_path': "",
-        'bpe_dropout': 0.0,
-        'vocabulary_path': "",
-        'vocabulary_threshold': 0,
-        'sp_model_path': "",
-        'sp_nbest_size': 0,
-        'sp_alpha': 0.1,
-        'joiner': "￭",
-        'joiner_annotate': False,
-        'joiner_new': False,
-        'spacer_annotate': False,
-        'spacer_new': False,
-        'case_feature': False,
-        'case_markup': False,
-        'no_substitution': False,
-        'preserve_placeholders': False,
-        'preserve_segmented_tokens': False,
-        'segment_case': False,
-        'segment_numbers': False,
-        'segment_alphabet_change': False,
-        'support_prior_joiners': False,
-        'segment_alphabet': []
-    }
-    fin = 'stdin'
-    fout = 'stdout'
-    num_threads = 1
-
-    usage = """usage: {} [-i FILE -o FILE -num_threads INT]  [tok_options]
-    -i: stdin
-    -o: stdout
-    -num_threads: 1
+    usage = """usage: {} -o file [-i FILE]+ [-symbols INT] [-min_frequency INT] [tok_options]
+    -i: (stdin) [save wildcards for files]
+    -o: 
+    -symbols: 32000
+    -min_frequency: 1
     -h: this message
 
   tok_options (See https://github.com/OpenNMT/Tokenizer for more details):
-""".format(sys.argv.pop(0),tokopts)
-    for k,v in tokopts.items():
+""".format(sys.argv.pop(0),t.tokopts)
+    for k,v in t.tokopts.items():
         usage += "    -{}: {}\n".format(k,v)
 
-    opt_strings = ['mode','bpe_model_path','vocabulary_path','sp_model_path','joiner']
-    opt_ints = ['vocabulary_threshold','sp_nbest_size']
-    opt_floats = ['bpe_dropout','sp_alpha']
-    opt_lists = ['segment_alphabet']
-    opt_bools = ['joiner_annotate','joiner_new','spacer_annotate','spacer_new','case_feature','case_markup','no_substitution','preserve_placeholders','preserve_segmented_tokens','segment_case','segment_numbers','segment_alphabet_change','support_prior_joiners']
-
+    sys.argv = t.updateOpts(sys.argv)
     while len(sys.argv):
         tok = sys.argv.pop(0)
         if tok=="-h":
             sys.stderr.write("{}\n".format(usage))
             sys.exit()
-
-        if tok[0] == '-':
-            tok = tok[1:] ### discard initial '-'
-        else:
-            sys.stderr.write('error: option {} must be passed with initial \'-\'\n'.format(tok))
-            sys.stderr.write("{}\n".format(usage))
-            sys.exit()
-
-        if tok in opt_strings and len(sys.argv):
-    	    tokopts[tok] = sys.argv.pop(0)
-        elif tok in opt_ints and len(sys.argv):
-    	    tokopts[tok] = int(sys.argv.pop(0))
-        elif tok in opt_floats and len(sys.argv):
-    	    tokopts[tok] = float(sys.argv.pop(0))
-        elif tok in opt_lists and len(sys.argv):
-    	    tokopts[tok] = sys.argv.pop(0).split(',')
-        elif tok in opt_bools:
-    	    tokopts[tok] = True
-        elif tok=="i" and len(sys.argv):
-            fin = sys.argv.pop(0)
-        elif tok=="o" and len(sys.argv):
+        elif tok=="-i" and len(sys.argv):
+            for f in glob.glob(sys.argv.pop(0)): 
+                fin.append(f)
+        elif tok=="-o" and len(sys.argv):
             fout = sys.argv.pop(0)
-        elif tok=="um_threadsn" and len(sys.argv):
-            num_threads = int(sys.argv.pop(0))        
+        elif tok=="-symbols" and len(sys.argv):
+            symbols = int(sys.argv.pop(0))        
+        elif tok=="-min_frequency" and len(sys.argv):
+            min_frequency = int(sys.argv.pop(0))        
         else:
             sys.stderr.write('error: unparsed {} option\n'.format(tok))
             sys.stderr.write("{}\n".format(usage))
             sys.exit()
 
-    tokenize(tokopts, fin, fout, num_threads)
+    if fout is None:
+        sys.stderr.error('option -o must be used\n')
+        sys.stderr.write("{}\n".format(usage))
+        sys.exit()        
+            
+    create_logger()
+            
+    l = pyonmttok.BPELearner(tokenizer=t.get_tokenizer(), symbols=symbols, min_frequency=min_frequency)
+
+    if len(fin) == 0:
+        logging.info('Read stdin')
+        for line in sys.stdin:
+            l.ingest(str(line.strip('\n')))
+    else:
+        for f in fin:
+            logging.info('Read {}'.format(f))
+            l.ingest_file(f)
+
+    logging.info('learning... symbols={} min_frequency={}'.format(symbols, min_frequency))
+    l.learn(fout)
+    logging.info('Done')
--- tokenise/tokenize.py
+++ tokenise/tokenize.py
@@ -4,8 +4,22 @@ import sys
 import os
 import six
 import pyonmttok
+import logging
 from tokenizer import tokenizer
 
+def create_logger(logfile=None, loglevel='debug'):
+    numeric_level = getattr(logging, loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        sys.stderr.write("Invalid log level={}\n".format(loglevel))
+        sys.exit()
+    if logfile is None or logfile == 'stderr':
+        logging.basicConfig(format='[%(asctime)s.%(msecs)03d] %(levelname)s %(message)s', datefmt='%Y-%m-%d_%H:%M:%S', level=numeric_level)
+        logging.info('Created Logger level={}'.format(loglevel))
+    else:
+        logging.basicConfig(filename=logfile, format='[%(asctime)s.%(msecs)03d] %(levelname)s %(message)s', datefmt='%Y-%m-%d_%H:%M:%S', level=numeric_level)
+        logging.info('Created Logger level={} file={}'.format(loglevel, logfile))
+
+        
 ################################################
 ### MAIN #######################################
 ################################################
@@ -44,11 +58,16 @@ if __name__ == '__main__':
             sys.stderr.write("{}\n".format(usage))
             sys.exit()
 
+    create_logger()
+            
     if fin is not None and fout is not None:
+        logging.info('Reading {} writing {}'.format(fin,fout))
         t.tokenize_file(fin, fout, num_threads)
 
     else:
+        logging.info('Reading stdin writing stdout')
         for line in sys.stdin:
             line = t.tokenize_line(str(line.strip('\n')))
             print("{}".format(" ".join(line)))
             
+    logging.info('Done')
