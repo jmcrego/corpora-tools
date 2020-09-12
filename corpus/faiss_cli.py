b3933a81131ec4a13bebadf9c58a0542b2cdc423
jmcrego <jmcrego@gmail.com>
2020-09-12 23:09:04 +0200

    new format

--- corpus/faiss_cli.py
+++ corpus/faiss_cli.py
@@ -202,7 +202,7 @@ All indexs start by 0
         tstart = timer()
         query = Infile(fquery, d=0, norm=True, max_vec=max_vec)
         results = indexfaiss.Query(query,k)
-        logging.info('Dumping {}-bests in {}'.format(k,fquery+'.'+tag))
+        logging.info('\t\tDumping {}-bests in {}'.format(k,fquery+'.'+tag))
         with open(fquery+'.'+tag, "w") as fout:
             for result in results: ### one line per query line
                 res = []
