26e0c73e018735aa3924a5310be296d3ab08bf3c
jmcrego <jmcrego@gmail.com>
2020-09-13 17:12:45 +0200

    add

---
+++ corpus/sim2prime.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+
+def progress(n_line):
+    if n_line%10000 == 0:
+        if n_line%100000 == 0:
+            sys.stderr.write("{}".format(n_line))
+        else:
+            sys.stderr.write(".")
+
+#####################################################################
+### MAIN ############################################################
+#####################################################################
+
+if __name__ == '__main__':
+
+    n = 1
+    t = 0.5
+    ranges = False
+    fdb_src = None
+    fdb_tgt = None
+    fq_src = None
+    fq_tgt = None
+    sep = '<[sep]>'
+    sep_st = '\t'
+    name = sys.argv.pop(0)
+    usage = '''usage: {} -db_src FILE -db_tgt FILE -q_src FILE -q_tgt FILE [-n INT] [-t FLOAT] < FSIM > FAUGMENTED
+   -db_src FILE : db file with src strings to output
+   -db_tgt FILE : db file with tgt strings to output
+   -q_src  FILE : query file with src strings
+   -q_tgt  FILE : query file with tgt strings
+   -ranges      : use score ranges
+   -n       INT : max n-best similar to output (default 1)
+   -t     FLOAT : min threshold to consider (default 0.5)
+   -h           : this help
+'''.format(name)
+
+    while len(sys.argv):
+        tok = sys.argv.pop(0)
+        if tok=="-h":
+            sys.stderr.write("{}".format(usage))
+            sys.exit()
+        elif tok=="-db_src" and len(sys.argv):
+            fdb_src = sys.argv.pop(0)
+        elif tok=="-db_tgt" and len(sys.argv):
+            fdb_tgt = sys.argv.pop(0)
+        elif tok=="-q_src" and len(sys.argv):
+            fq_src = sys.argv.pop(0)
+        elif tok=="-q_tgt" and len(sys.argv):
+            fq_tgt = sys.argv.pop(0)
+        elif tok=="-n" and len(sys.argv):
+            n = int(sys.argv.pop(0))
+        elif tok=="-t" and len(sys.argv):
+            t = float(sys.argv.pop(0))
+        elif tok=="-ranges":
+            ranges = True
+        else:
+            sys.stderr.write('error: unparsed {} option\n'.format(tok))
+            sys.stderr.write("{}".format(usage))
+            sys.exit()
+
+    if fdb_src is None:
+        sys.stderr.write('error: missing -db_src option\n')
+        sys.stderr.write("{}".format(usage))
+        sys.exit()
+
+    if fq_src is None:
+        sys.stderr.write('error: missing -q_src option\n')
+        sys.stderr.write("{}".format(usage))
+        sys.exit()
+
+    sys.stderr.write('Reading {}\n'.format(fdb_src))
+    with open(fdb_src,'r') as f:
+        DB_src = [x.rstrip() for x in f]
+    sys.stderr.write('Read fdb={} with {} lines\n'.format(fdb_src, len(DB_src)))
+
+    if fdb_tgt is not None:
+        sys.stderr.write('Reading {}\n'.format(fdb_tgt))
+        with open(fdb_tgt,'r') as f:
+            DB_tgt = [x.rstrip() for x in f]
+        if len(DB_tgt) != len(DB_src):
+            sys.stderr.write('error: erroneous number of lines in fdb_tgt {}'.format(len(DB_tgt)))
+            sys.exit()
+        sys.stderr.write('Read fdb={} with {} lines\n'.format(fdb_tgt, len(DB_tgt)))
+
+    sys.stderr.write('Reading {}\n'.format(fq_src))
+    with open(fq_src,'r') as f:
+        Q_src = [x.rstrip() for x in f]
+    sys.stderr.write('Read fq={} with {} lines\n'.format(fq_src, len(Q_src)))
+
+    if fq_tgt is not None:
+        sys.stderr.write('Reading {}\n'.format(fq_tgt))
+        with open(fq_tgt,'r') as f:
+            Q_tgt = [x.rstrip() for x in f]
+        sys.stderr.write('Read fq={} with {} lines\n'.format(fq_tgt, len(Q_tgt)))
+        if len(Q_tgt) != len(Q_src):
+            sys.stderr.write('error: erroneous number of lines in fq_tgt {}'.format(len(Q_tgt)))
+            sys.exit()
+
+    
+    for n_query, line in enumerate(sys.stdin):
+        line = line.rstrip()
+
+        if line == '':
+            print('')
+            continue
+        
+        #print(line)
+        toks = line.split('\t')
+        if len(toks) % 2 != 0:
+            sys.stderr.write('error: unparsed line {}'.format(line))
+
+        src_sentences = []
+        tgt_sentences = []
+        while len(toks):
+            score = float(toks.pop(0))
+            n_db = int(toks.pop(0))
+
+            if score < t:
+                break
+
+            if ranges:
+                tag = '<[{}]>'.format(str(score)[0:3])
+            else:
+                tag = sep
+
+            src_sentences.append(tag + ' ' + DB_src[n_db])
+            if fdb_tgt is not None:
+                tgt_sentences.append(tag + ' ' + DB_tgt[n_db])
+
+            if len(src_sentences) >= n:
+                break
+
+        if len(src_sentences) == 0:
+            print('')
+            continue
+            
+        ### add query sentence/s
+        if fq_tgt is None:
+            src_sentences.append(sep + ' ' + Q_tgt[n_query])
+        else:
+            src_sentences.append(sep + ' ' + Q_src[n_query])
+            tgt_sentences.append(sep + ' ' + Q_tgt[n_query])
+            
+        if len(tgt_sentences) == 0:
+            print(' '.join(src_sentences))
+        else:
+            print(' '.join(src_sentences) + '\t' + ' '.join(tgt_sentences))
+
