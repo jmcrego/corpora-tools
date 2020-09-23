b760b08a227683fd730aabdeeab3ab000bf4f74c
jmcrego <jmcrego@gmail.com>
2020-09-23 19:33:49 +0200

    light

--- corpus/sim2prime.py
+++ corpus/sim2prime.py
@@ -287,9 +287,11 @@ if __name__ == '__main__':
             tag = get_separator(use_range, score) #+ str(score)
             if fdb_src is not None: ### PRIMING: augment source and target sides
                 src_similar = tag + DB_src[n_db].split() #['※', 'the', 'house']
+                print(src_similar)
                 src_similars.append(src_similar) #src_similars[0] is the closest to curr_src
 
                 tgt_similar = tag + DB_tgt[n_db].split() #['※', 'la', 'maison']
+                print(tgt_similar)
                 tgt_similars.append(tgt_similar)
             else: ### BULTE et al: augment source side with DB_tgt
                 tgt_similar = tag + DB_tgt[n_db].split() #['※', 'la', 'maison']
