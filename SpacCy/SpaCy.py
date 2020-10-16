eda8072666f3a909baeade971b54391c124944de
jmcrego <jmcrego@gmail.com>
2020-10-16 08:26:54 +0000

    ligth

--- SpacCy/SpaCy.py
+++ SpacCy/SpaCy.py
@@ -2,7 +2,7 @@
 
 import sys
 import io
-#import glob
+import math
 import spacy
 import logging
 
@@ -30,11 +30,13 @@ class Args():
         self.joiner = '￨'
         self.log_file = 'stderr'
         self.log_level = 'info'
+        self.bucket = 1000
         self.prog = argv.pop(0)
-        self.usage = '''usage: {} [-model MODEL] [-data FILE] [-joiner STRING] [-log_file FILE] [log_level LEVEL]
+        self.usage = '''usage: {} [-model MODEL] [-data FILE] [-joiner STRING] [-bucket_size INT] [-log_file FILE] [log_level LEVEL]
    -data       FILE : input file                            (stdin)
    -model     MODEL : spacy model                           (en) [equivalent to en_core_web_sm]
    -joiner   STRING : string to join linguistic features    (￨)
+   -bucket      INT : bucket size                           (1000)
    -log_file   FILE : log file (use stderr for STDERR)      (stderr)
    -log_level LEVEL : debug, info, warning, critical, error (debug) 
    -h               : this help
@@ -50,6 +52,7 @@ class Args():
             if (tok=="-joiner" and len(argv)): self.joiner = argv.pop(0)
             elif (tok=="-data" and len(argv)): self.data = argv.pop(0)
             elif (tok=="-model" and len(argv)): self.model = argv.pop(0)
+            elif (tok=="-bucket" and len(argv)): self.bucket = int(argv.pop(0))
             elif (tok=="-log_file" and len(argv)): self.log_file = argv.pop(0)
             elif (tok=="-log_level" and len(argv)): self.log_level = argv.pop(0)
             elif (tok=="-h"):
@@ -68,8 +71,6 @@ if __name__ == "__main__":
     args = Args(sys.argv) #creates logger
     #nlp = spacy.load(args.model, disable=["parser"])
     nlp = spacy.load(args.model)
-    #nlp.add_pipe(length_component, first=True) # Add the component first in the pipeline
-
     logging.info('pipeline: {}'.format(nlp.pipe_names)) #print(nlp.pipeline)
 
     TEXTS = []
@@ -79,38 +80,55 @@ if __name__ == "__main__":
     else:
         for l in sys.stdin:
             TEXTS.append(l.rstrip())
-    logging.info('read {} with {} lines'.format(args.data,len(TEXTS)))
 
-    docs = list(nlp.pipe(TEXTS))
-    logging.info('processed {} lines'.format(len(docs)))
+    nbuckets = math.ceil(len(TEXTS) / args.bucket)
+    nlines = len(TEXTS)
+    logging.info('read {} with {} lines, {} buckets'.format(args.data, nlines, nbuckets))
 
-    while len(docs):
-        doc = docs.pop(0)
-        sentence = []
-        start_char2i = {}
-        end_char2i = {}
-        for token in doc:
-            start_char2i[token.idx] = token.i
-            feats = []
-            feats.append(str(token.i))
-            feats.append(token.text)
-            feats.append(token.lemma_)
-            feats.append(token.tag_)
-            #feats.append(token.pos_)
-            #feats.append(token.dep_)
-            #feats.append(token.ent_iob_)
-            #feats.append(token.ent_type_)
-            sentence.append(args.joiner.join(feats))
+    nbucket = 0
+    nline = 0
+    while len(TEXTS):
+        nbucket += 1        
+        if len(TEXTS) >= args.bucket:
+            BUCKET = TEXTS[:args.bucket]
+            del TEXTS[:args.bucket]
+        else:
+            BUCKET = list(TEXTS)
+            del TEXTS[:len(TEXTS)]
+    
+        docs = list(nlp.pipe(BUCKET))
+        while len(docs): ### docs are lines
+            nline += 1
+            doc = docs.pop(0)
+            sentence = []
+            start_char2i = {}
+            end_char2i = {}
+            for i,token in enumerate(doc):
+                start_char2i[token.idx] = token.i
+                feats = []
+                if token.i != i:
+                    logging.error('token.i != i in line {}'.format(nline))
+                #feats.append(str(token.i))
+                feats.append(token.text)
+                #feats.append(token.lemma_)
+                feats.append(token.tag_)
+                #feats.append(token.pos_)
+                #feats.append(token.dep_)
+                #feats.append(token.ent_iob_)
+                #feats.append(token.ent_type_)
+                sentence.append(args.joiner.join(feats))
 
-        Ents = []
-        for ent in doc.ents:
-            i = start_char2i[ent.start_char]
-            Ents.append("Ent:{}[{},{}] {}".format(ent.label_, i, i + len(ent.text.split()) - 1, ent.text))
-
-        NPs = []
-        for chk in doc.noun_chunks:
-            i = start_char2i[chk.start_char]
-            NPs.append("NP[{},{}] {}".format(i,i + len(chk.text.split()) - 1, chk.text))
+            oline = ' '.join(sentence)
             
-        print(' '.join(sentence) + '\t' + '\t'.join(Ents) + '\t' + '\t'.join(NPs))
+            for ent in doc.ents:
+                i = start_char2i[ent.start_char]
+                oline += "\t" + "Ent[{},{}]:{} {}".format(i, i + len(ent.text.split()) - 1, ent.label_, ent.text)
+                
+            for chk in doc.noun_chunks:
+                i = start_char2i[chk.start_char]
+                oline += "\t" + "NP[{},{}] {}".format(i, i + len(chk.text.split()) - 1, chk.text)
+            
+            print(oline)
+
+        logging.info('processed {}/{} lines, {}/{} buckets'.format(nline, nlines, nbucket, nbuckets))
 
