0351fd1d06a707e54ffce868f3a22d66a9476441
Josep Crego <jmcrego@gmail.com>
2021-12-01 14:20:21 +0100

    light

--- simalign/align.py
+++ simalign/align.py
@@ -12,6 +12,7 @@ import logging
 import argparse
 import numpy as np
 import edit_distance
+from copy import deepcopy
 from sklearn.metrics.pairwise import cosine_similarity
 from transformers import BertModel, BertTokenizer, XLMModel, XLMTokenizer, RobertaModel, RobertaTokenizer, XLMRobertaModel, XLMRobertaTokenizer
 
@@ -46,80 +47,95 @@ def get_tokenizer_model(model,device):
     return tokenizer, emb_model
         
 def matrix(usim, lsim, ltgt, x, y):
-    #lsim : [nsim]
-    #ltgt : [ntgt]
-    #x : [ntgt, nsim]
-    #y : [ntgt]
-    lsim = [str(usim[s])+":"+lsim[s] for s in range(len(lsim))]
-    maxl = max([len(x) for x in lsim+ltgt] + [4]) + 1
+    lsim = deepcopy(lsim) #lsim : [nsim]
+    ltgt = deepcopy(ltgt) #ltgt : [ntgt]
+    size_score = 4 #size of each score {:.2f}
+    size_cell = max([len(x) for x in lsim+ltgt] + [size_score]) + 1
     for s in range(len(lsim)):
-        lsim[s] += " "*(maxl - len(lsim[s]))
+        lsim[s] += " "*(size_cell - len(lsim[s]))
     for t in range(len(ltgt)):
-        ltgt[t] += " "*(maxl - len(ltgt[t]))
+        ltgt[t] += " "*(size_cell - len(ltgt[t]))
+    lusim = []
+    for s in range(len(usim)):
+        lusim.append(str(usim[s]) + " "*(size_cell - 1))
 
-    print(' '*maxl+''.join(lsim)+'ATTENTION')
+    logging.debug(' '*size_cell+''.join(lusim))
+    logging.debug(' '*size_cell+''.join(lsim)+'ATTENTION')
     for t in range(x.shape[0]):
-        ltgt_t = [ltgt[t]]
+        ltgt_t = []
         for s in range(x.shape[1]):
-            u = "{:.2f}".format(x[t,s]) if x[t,s] > 0.0 else ""
-            ltgt_t.append(u+ " "*(maxl - len(u)))
-        ltgt_t.append("{:.7f}".format(y[t]))
-        print(''.join(ltgt_t))
+            ltgt_t.append("{:.2f}".format(x[t,s]) + " "*(size_cell - size_score))
+        logging.debug(ltgt[t] + ''.join(ltgt_t) + "{:.7f}".format(y[t]))
 
 
-def reformat(ids,embed):
-    '''
-    discard tags (PAD, CLS, SEP)
-    rejoin sub-tokens
-    '''
-    PAD = 0
-    CLS = 101
-    SEP = 102
-
+def join_avg_subtokens(ids,embed):
     lids = [tokenizer.decode([s]) for s in ids]
     lstr = []
     lemb = []
-    curr = 1
+    curr = 0
     while True:
-        if ids[curr] == SEP or ids[curr] == PAD: ### end of sequence
+        if curr == ids.shape[0]: ### end of sequence
             break
         last = curr + 1
-        while ids[last] != SEP and ids[last] != PAD and lids[last].startswith("##"):
+        while last < ids.shape[0] and lids[last].startswith("##"):
             last += 1
+        ###
+        ### join tokens from curr to last-1
+        ###
         lstr.append(''.join(lids[curr:last]).replace('##',''))
         if embed is not None:
             lemb.append(np.average(embed[curr:last], axis=0))
         curr = last
-    if embed is not None:
-        lemb = np.asarray(lemb)    
-    return lstr, lemb
+    return lstr, np.asarray(lemb)
 
-def process_batch(batch_src, batch_sim, batch_tgt, tokenizer, emb_model, args):
-    inputs_src = tokenizer(batch_src, is_split_into_words=False, padding=True, truncation=True, return_tensors="pt")
-    inputs_sim = tokenizer(batch_sim, is_split_into_words=False, padding=True, truncation=True, return_tensors="pt")
-    inputs_tgt = tokenizer(batch_tgt, is_split_into_words=False, padding=True, truncation=True, return_tensors="pt")
-    hidden_sim = emb_model(**inputs_sim.to(device))["hidden_states"]
-    hidden_tgt = emb_model(**inputs_tgt.to(device))["hidden_states"]
-    if args.layer >= len(hidden_sim):
-        raise ValueError("Specified to take embeddings from layer {}, but model has only {} layers.".format(args.layer,len(hidden_sim)))
-    hidden_sim = hidden_sim[args.layer] # [bs, nsim, ed]
-    hidden_tgt = hidden_tgt[args.layer] # [bs, ntgt, ed]
-    for i in range(hidden_sim.shape[0]): ### for each sentence
-        lsrc, _ = reformat(inputs_src['input_ids'][i].detach().numpy(),None)
-        lsim, embed_sim = reformat(inputs_sim['input_ids'][i].detach().numpy(), hidden_sim[i])
-        usim = np.asarray(unrelated(lsrc,lsim)).transpose()
-        ltgt, embed_tgt = reformat(inputs_tgt['input_ids'][i].detach().numpy(), hidden_tgt[i])
-        logging.debug('SRC: '+str(lsrc))
-        logging.debug('SIM: '+str(lsim))
-        logging.debug('UNR: '+str(usim))
-        logging.debug('TGT: '+str(ltgt))
-        cosine = (cosine_similarity(embed_tgt, embed_sim) + 1.0 ) / 2.0 # [ntgt, nsim], ranges [0,+1] instead of [-1,+1]
-        x = scipy.special.softmax(cosine/args.temperature, axis=1) ### [ntgt, nsim] softmax with temperature
-        x = np.maximum(np.zeros_like(x), np.minimum(np.ones_like(x), args.slope*(x-args.min))) ### [ntgt, nsim] activation function
-        y = np.matmul(x, np.expand_dims(usim,axis=1)).squeeze() #[ntgt, nsim] x [nsim, 1] = [ntgt]
-        logging.debug('ATT: '+str(y))
-        matrix(usim, lsim, ltgt, x, y)
+def process_triplet(src, sim, tgt, tokenizer, emb_model, args):
+    ### (sub-0tokenize src/sim/tgt sentences
+    src_stok = tokenizer(src, is_split_into_words=True, padding=False, truncation=False, return_tensors="pt", add_special_tokens=False, verbose=False)
+    sim_stok = tokenizer(sim, is_split_into_words=True, padding=False, truncation=False, return_tensors="pt", add_special_tokens=False, verbose=False)
+    tgt_stok = tokenizer(tgt, is_split_into_words=True, padding=False, truncation=False, return_tensors="pt", add_special_tokens=False, verbose=False)
+
+    ### get idx's of src/sim/tgt
+    src_stok_ids = np.asarray(src_stok['input_ids'][0]) #[nsrc]
+    sim_stok_ids = np.asarray(sim_stok['input_ids'][0]) #[nsim]
+    tgt_stok_ids = np.asarray(tgt_stok['input_ids'][0]) #[ntgt]
+
+    ### apply the model on tokenized sim/tgt sentences
+    sim_stok_hidden = emb_model(**sim_stok.to(device))["hidden_states"]
+    tgt_stok_hidden = emb_model(**tgt_stok.to(device))["hidden_states"]
+
+    ### get the hidden layer of sim/tgt
+    if args.layer >= len(sim_stok_hidden):
+        raise ValueError("Specified to take embeddings from layer {}, but model has only {} layers.".format(args.layer,len(sim_stok_hidden)))
+    sim_stok_hidden = np.asarray(sim_stok_hidden[args.layer].squeeze()) # [nsim, ed]
+    tgt_stok_hidden = np.asarray(tgt_stok_hidden[args.layer].squeeze()) # [ntgt, ed]
+
+    ### rejoin subtokenized words and average their embeddings
+    src_tok, _              = join_avg_subtokens(src_stok_ids, None)            # [nsrc] 
+    sim_tok, sim_tok_hidden = join_avg_subtokens(sim_stok_ids, sim_stok_hidden) # [nsim] [nsim, ed]
+    tgt_tok, tgt_tok_hidden = join_avg_subtokens(tgt_stok_ids, tgt_stok_hidden) # [ntgt] [ntgt, ed]
+
+    logging.debug('SRC: '+' '.join(src_tok))
+    logging.debug('TGT: '+' '.join(tgt_tok))
+
+    if len(tgt_tok) == 0:
+        print(' '.join(src_tok))
+
+    ### find unrelated words between sim/src sentences
+    sim_tok_unrelated = unrelated(src_tok,sim_tok) #[ntgt]
+    logging.debug('SIM: '+' '.join([str(sim_tok_unrelated[i])+":"+sim_tok[i] for i in range(len(sim_tok))]) )
     
+    x = cosine_similarity(tgt_tok_hidden, sim_tok_hidden) # [ntgt, nsim]
+    x = (x + 1.0) / 2.0 #ranges [0,+1] instead of [-1,+1]
+    x = scipy.special.softmax(x/args.temperature, axis=1) # [ntgt, nsim] softmax with temperature
+    x = np.maximum(np.zeros_like(x), np.minimum(np.ones_like(x), args.slope*(x-args.min))) ### [ntgt, nsim] activation function
+    w = np.expand_dims(np.asarray(sim_tok_unrelated), axis=1) #[nsim,1]
+    y = np.matmul(x, w).squeeze() #[ntgt, nsim] x [nsim, 1] = [ntgt]
+    if args.log == 'debug':
+        matrix(sim_tok_unrelated, sim_tok, tgt_tok, x, y)
+    logging.debug('ATT: '+' '.join(["{}:{:.2f}".format(tgt_tok[i],y[i]) for i in range(len(tgt_tok))]))
+    print(' '.join(src_tok) + "\t" + ' '.join(tgt_tok) + "\t" + ' '.join(["{:.6f}".format(y[i]) for i in range(y.shape[0])]))
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('fsrc', help='SOURCE file (one sentence per line)')
@@ -129,7 +145,6 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--slope', type=float, default=1.0, help='Slope for activation function (def 1.0)')
     parser.add_argument('-m', '--min', type=float, default=0.0, help='Minimum for activation function (def 0.0)')
     parser.add_argument('-l', '--layer', type=int, default=8, help='Encoder layer to use (def 8)')
-    parser.add_argument('-b', '--batch_size', type=int, default=1, help='Batch size (def 1)')
     parser.add_argument('-log', default='info', help="Logging level [debug, info, warning, critical, error] (info)")
     args = parser.parse_args()
     create_logger('stderr',args.log)
@@ -139,19 +154,13 @@ if __name__ == '__main__':
     tokenizer, emb_model = get_tokenizer_model(model,device)
 
     with torch.no_grad():
-        batch_src = []
-        batch_sim = []
-        batch_tgt = []
         with codecs.open(args.fsrc, 'r', 'utf-8') as fsrc, codecs.open(args.fsim, 'r', 'utf-8') as fsim, codecs.open(args.ftgt, 'r', 'utf-8') as ftgt:
-            for lsrc, lsim, ltgt in zip(fsrc, fsim, ftgt):
-                batch_src.append(lsrc.strip())
-                batch_sim.append(lsim.strip())
-                batch_tgt.append(ltgt.strip())
-                if len(batch_src) == args.batch_size:
-                    process_batch(batch_src, batch_sim, batch_tgt, tokenizer, emb_model, args)
-                    batch_src = []
-                    batch_sim = []
-                    batch_tgt = []                    
-            if len(batch_src):
-                process_batch(batch_src, batch_sim, batch_tgt, tokenizer, emb_model, args)
+            for src, sim, tgt in zip(fsrc, fsim, ftgt):
+                process_triplet(src, sim, tgt, tokenizer, emb_model, args)
+
+
+
+
+
+
 
